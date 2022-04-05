14bb445a0586922648a0c543954bbd4fd61df18d
Shady-Rouk <faroukbalogun@techexchange.in>
2022-04-05 13:26:47 +0000

    inserting sample document to database

--- Work Connect/model.py
+++ Work Connect/model.py
@@ -5,3 +5,7 @@ client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ.get('CS_Password'
 db = client.ProjectDB
 # db.create_collection('Jobs')
 jobsDB = db.companies
+
+# jobsDB.insert_one(
+#     {"company_name": "Company X", "job_scope": {"tech": "www.tech.com", "business": "www.business.com", "finance": "www.finance.com"}}
+# )
