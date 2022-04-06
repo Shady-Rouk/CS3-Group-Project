92bc1a07a437b2327fcd118960c6e82c7de3aa22
Shady-Rouk <faroukbalogun@techexchange.in>
2022-04-06 21:52:48 +0000

    sample

--- Work Connect/seed_library.py
+++ Work Connect/seed_library.py
@@ -1,2 +1,6 @@
 #file will have the list of dictionaries for each company
 #will be used to populate the database once all data is collected
+
+# companies = [
+#      {"company_name": "Company X", "job_scope": {"tech": "www.tech.com", "business": "www.business.com", "finance": "www.finance.com"}}
+# ]
