00f3154e48ef78869baba347b705880d5c938882
Prem@040456 <prem.oli@bison.howard.edu>
2022-04-13 10:06:37 -0400

    worked on the layout file for template Inheritance, need to work on some of the styling which I will do after our this week meeting. Moreover, created a form to take job scope as input which needs to be handled in database in someway which I has no knowledge till now.

--- Work Connect/app.py
+++ Work Connect/app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, request, url_for, redirect, session 
+from flask import Flask, render_template, request, url_for, redirect, session, request
 
 app = Flask(__name__) 
 
-@app.route("/", methods = ["GET"]) 
-def index(): 
-    return render_template("index.html") 
-    
-@app.route("/home/", methods = ["GET"]) 
-def home(): 
-    return redirect(url_for("index")) 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/home", methods = ['GET','POST']) 
+def home():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("result.html")
+    else:
+        return render_template("index.html")
+
 
 @app.route("/about", methods = ["GET"])
 def about():
     return render_template("about.html")
-    
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        return render_template("result.html")
+
+
     
 if __name__ == "__main__": 
     app.run(debug = True)
