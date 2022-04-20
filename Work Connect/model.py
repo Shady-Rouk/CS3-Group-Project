97430b24f0fac8431718894f4d16ebe5f986d99d
Shady-Rouk <faroukbalogun@techexchange.in>
2022-04-20 01:30:31 +0000

    fixing db access

--- Work Connect/model.py
+++ Work Connect/model.py
@@ -1,7 +1,7 @@
 import pymongo
 import os
 
-client = pymongo.MongoClient("mongodb+srv://admin:"+os.environ.get('CS_Password')+"@cluster0.knoxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://admin:X77ThUxLgbBxLOsr@cluster0.knoxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.ProjectDB
 # db.create_collection('Jobs')
 jobsDB = db.companies
