a70c0a873480bdcbbd9b8f8e2068a7d8d6eab0a7
Shady-Rouk <faroukbalogun@techexchange.in>
2022-04-29 16:15:08 -0400

    wrote function to push data to MongoDB collection

--- Work Connect/model.py
+++ Work Connect/model.py
@@ -1,5 +1,6 @@
 import pymongo
 import os
+from seed_library import companies
 
 client = pymongo.MongoClient("mongodb+srv://admin:X77ThUxLgbBxLOsr@cluster0.knoxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.ProjectDB
@@ -13,3 +14,6 @@ jobsDB = db.companies
 def get_details_from_db(scope):
     sponsors = jobsDB.find({"job_scope."+str(scope): {"$exists": 1}} , {"company_name":1, "job_scope."+str(scope):1, "_id":0})
     return sponsors
+
+# def seed():
+#     jobsDB.insert_many(companies)
